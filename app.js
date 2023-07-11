ec66655a58914002062086b0dc823d74fa10dae7
rwxp <sdrivert@hotmail.com>
2023-07-11 15:32:09 -0500

    Dependencies and db connection added

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+const morgan = require('morgan');
+const exphbs = require('express-handlebars');
+//Load config
+dotenv.config({ path: './config/config.env' });
+
+connectDB();
+const app = express();
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+const PORT = process.env.PORT || 5000;
+
+app.listen(
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+);
