a660c38ea1d5cfbcba9de3246bc77c67e3034d80
rwxp <sdrivert@hotmail.com>
2023-07-12 08:25:30 -0500

    Auth with google added v1

--- app.js
+++ app.js
@@ -1,17 +1,42 @@
+const path = require('path');
 const express = require('express');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
 const morgan = require('morgan');
 const exphbs = require('express-handlebars');
+const passport = require('passport');
+const session = require('express-session');
 //Load config
 dotenv.config({ path: './config/config.env' });
 
+//Passport config
+require('./config/passport')(passport);
 connectDB();
 const app = express();
 
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
+//Handlebars:
+app.engine('.hbs', exphbs.engine({ defaultLayout: 'main', extname: '.hbs' }));
+app.set('view engine', '.hbs');
+
+app.use(
+  session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+//Static folder
+app.use(express.static(path.join(__dirname, 'public')));
+//Here we import router from routes/index
+app.use('/', require('./routes/index'));
+app.use('/auth', require('./routes/auth'));
 const PORT = process.env.PORT || 5000;
 
 app.listen(
