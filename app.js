044e89f3cb455a31fff8a5e58f8517030338edda
rwxp <sdrivert@hotmail.com>
2023-07-27 19:57:35 -0500

    Story editing added and edit dashboard button

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const exphbs = require('express-handlebars');
 const passport = require('passport');
 const session = require('express-session');
 const MongoStore = require('connect-mongo')(session);
+const methodOverride = require('method-override');
 //Load config
 dotenv.config({ path: './config/config.env' });
 
@@ -19,17 +20,34 @@ const app = express();
 //Middleware (Body Parser) to accept form data.
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+//Method override for delete put request.
+app.use(
+  methodOverride(function (req, res) {
+    if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+      // look in urlencoded POST bodies and delete it
+      var method = req.body._method;
+      delete req.body._method;
+      return method;
+    }
+  })
+);
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
 //Handlebars helpers
-const { formatDate, stripTags, truncate, editIcon } = require('./helpers/hbs');
+const {
+  formatDate,
+  stripTags,
+  truncate,
+  editIcon,
+  select,
+} = require('./helpers/hbs');
 //Handlebars:
 app.engine(
   '.hbs',
   exphbs.engine({
-    helpers: { formatDate, stripTags, truncate, editIcon },
+    helpers: { formatDate, stripTags, truncate, editIcon, select },
     defaultLayout: 'main',
     extname: '.hbs',
   })
